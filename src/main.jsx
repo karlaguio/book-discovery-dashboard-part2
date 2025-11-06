6bcd34f3c1948727bf2eb95a8eb3c27ae1d8645b
Karla Maria Guio Cortes <155421453+karlaguio@users.noreply.github.com>
2025-11-05 22:43:18 -0300

    Fix: Add basename to BrowserRouter for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/book-discovery-dashboard-part2">
       <App />
     </BrowserRouter>
   </StrictMode>,
